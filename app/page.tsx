71abb7148fa69919b72ce8232419d16efe6ed1cc
Adrian Morales <adriangtoff@gmail.com>
2026-02-11 23:52:09 -0600

    siuu

--- app/page.tsx
+++ app/page.tsx
@@ -10,9 +10,9 @@ export default function Home() {
   const [showQuestion, setShowQuestion] = useState(false);
   const [screenWidth, setScreenWidth] = useState(1200);
   const audioRef = useRef<HTMLAudioElement | null>(null);
-const [soundEnabled, setSoundEnabled] = useState(false);
-const [accepted, setAccepted] = useState(false);
-const [noPosition, setNoPosition] = useState({ x: 0, y: 80 });
+  const [soundEnabled, setSoundEnabled] = useState(false);
+  const [accepted, setAccepted] = useState(false);
+  const [noPosition, setNoPosition] = useState({ x: 0, y: 80 });
 
 
   const moments = [
@@ -20,14 +20,14 @@ const [noPosition, setNoPosition] = useState({ x: 0, y: 80 });
     id: 1,
     title: "Nuestro primer beso",
     description:
-      "Ese día yo estaba nervioso y tú ya estabas desesperada porque yo estoy biien bueno… jajajjakja no te creas. Sí estaba nervioso porque, aunque tú digas que no, verdaderamente me gustabas muchísimo. Te me hacías una mujer increíblemente bella y especial.",
+      "Ese día yo estaba nervioso y tú ya estabas desesperada porque yo estoy bien bueno… jajajjakja no te creas, este día yo estaba muy nervioso porque ya queria conocerte, tu de verdad me gustaste desde el primer día que te ví y cuando Edwin me decía de tu cumpleaños y que yo estaba invitado y asi, la verdad me emocionaba muchooo, porque tu estas preciosa, asi que saber que yo Tambien te gustaba fue muy lindo, ese día pasaron cositas que casi hacen que no fuera a tu fiesta, pero no pensaba dejar pasar la oportunidad de conocerte, ya lo del beso fue algo que para ser sinceros no me esperaba pero me gusto mucho.",
     imageUrl: "/moments/1.jpeg",
   },
   {
     id: 2,
     title: "Nuestra primera cita",
     description:
-      "Este día fue el comienzo de toda nuestra historia. Aunque la cagué y llegué muy tarde por ti, me diste chance 😂. Yo no sabía ni qué decirte de los nervios, pero te veías tan preciosa y deslumbrante como siempre… y ahí quedé completamente flechado.",
+      "Este día fue el comienzo de toda nuestra historia. Aunque la cagué y llegué muy tarde por ti, me diste chance. Yo no sabía ni qué decirte de los nervios, pero la conversación y el ambiente lo senti como si te conociera de toda la vida, aunque muchas cosas no te entendia aakjaja, y yo se que fui muy coqueto contigo, pero que sepas que todo eso lo practique antes de verte jakajkja btw te veías tan preciosa y deslumbrante como siempre… y ahí quedé completamente flechado.",
     imageUrl: "/moments/2.jpeg",
   },
   {
@@ -41,14 +41,14 @@ const [noPosition, setNoPosition] = useState({ x: 0, y: 80 });
     id: 4,
     title: "Nuestro primer 14 de febrero",
     description:
-      "Fue un día muy bonito a pesar de todo lo que estaba pasando en ese momento. Disfruté muchísimo tu compañía y, sin darme cuenta, cada día me enamoraba más de ti.",
+      "Fue un día muy bonito a pesar de todo lo que estaba pasando en ese momento, te agradezco mucho por darme la oportunidad y por creerme aunque todo el mundo te decía lo contrario. Disfruté muchísimo tu compañía y fue la primera vez que un 14 de febrero me emocionó , sin darme cuenta, cada día me enamoraba más de ti y de tus ojitos tan hermosos.",
     imageUrl: "/moments/4.jpeg",
   },
   {
     id: 5,
     title: "Cuando te pedí que fueras mi novia",
     description:
-      "Yo ya tenía claro que te amaba y que necesitaba tenerte en mi vida. Quería esforzarme y darte todo lo que estuviera en mis manos para hacerte sentir amada todos los días de tu vida.",
+      "Yo ya tenía claro que te amaba y que necesitaba tenerte en mi vida, ya amaba cada cosa que hacías, tu olor, tu voz, tus corajes, tus chistes y basicamente todo de tí, planeé durante semanas la manera en la que te iba a pedir, las canciones que pondría e incluso el ramo que queria darte. Quiero esforzarme y darte todo lo que este en mis manos para hacerte sentir amada todos los días de tu vida.",
     imageUrl: "/moments/5.jpeg",
   },
   {
@@ -69,7 +69,7 @@ const [noPosition, setNoPosition] = useState({ x: 0, y: 80 });
     id: 8,
     title: "Jarwuelin juntitos",
     description:
-      "Nuestro primer Halloween juntos terminó siendo una pedota masiva 😂. Me di cuenta de cuánto disfruto salir de fiesta contigo y terminar la noche llegando a dormir abrazaditos, piel con piel 🎃.",
+      "Nuestro primer Halloween juntos terminó siendo una pedota masiva. disfruto demasiado salir de fiesta contigo y conectar de la manera en la que conectamos andando de fiesta para luego terminar la noche llegando a dormir abrazaditos, piel con piel.",
     imageUrl: "/moments/8.jpeg",
   },
   {
@@ -83,21 +83,21 @@ const [noPosition, setNoPosition] = useState({ x: 0, y: 80 });
     id: 10,
     title: "Tus dos amores más grandes",
     description:
-      "Ese día fue increíble. Después de mil fotos y videos del Romy, por fin se me hizo conocerlo y pasearlo contigo. Verlos juntos me hizo muy feliz 🐶❤️.",
+      "Ese día fue increíble. Después de mil fotos y videos del Romy, por fin se me hizo conocerlo y pasearlo contigo, es un momento que atesoro bastante porque yo se cuanto amas a romy y haberlo conocido fue muy bello, ademas de obvio estar contigo todo el día 🐶❤️.",
     imageUrl: "/moments/10.jpeg",
   },
   {
     id: 11,
     title: "Tu cumpleaños",
     description:
-      "Ese día te veías hermosa, como siempre. Mientras planeaba todo, me di cuenta de cuánto sigo deseando hacerte feliz y ver esa sonrisa tuya que tanto amo 🎂.",
+      "Ese día te veías hermosa, como siempre. pase varios días planeando que hacerte y mas porque no te decidias si hacer algo o no jjakjakaj, disfrute cada momento de planear tu día, disfrute el organizarme con tu familia y dejar todo listo para que tu solo disfrutaras y ver esa sonrisa tuya que tanto amo, mientras yo este vivo prometo hacer que todos tus cumpleaños sean hermosos 🎂.",
     imageUrl: "/moments/11.jpeg",
   },
   {
     id: 12,
     title: "Mi foto favorita",
     description:
-      "Esta es mi foto favorita porque fue de nuestra primera sesión de fotitos juntos. Cada vez que la veo recuerdo lo mucho que amo estar contigo 📸❤️.",
+      "Esta es mi foto favorita porque fue de nuestra primera sesión de fotitos juntos. Cada vez que la veo recuerdo como comenzo nuestra historia de amor, de una manera tan natural y como todo se fue dando poco a poco 📸❤️.",
     imageUrl: "/moments/12.jpeg",
   },
 ];
@@ -131,11 +131,11 @@ const [noPosition, setNoPosition] = useState({ x: 0, y: 80 });
       </motion.div>
 
       <h1 className="text-4xl md:text-5xl font-bold text-white mb-6">
-        Activa el sonido
+        Hola mi amor, este año quise hacer algo especial para ti.
       </h1>
 
       <p className="text-xl md:text-2xl text-white/90 mb-10">
-        💖 Para una mejor experiencia 💖
+        💖 Tu jamás vas a ser espectadora 💖
       </p>
 
       <motion.button
@@ -150,7 +150,7 @@ const [noPosition, setNoPosition] = useState({ x: 0, y: 80 });
           setSoundEnabled(true);
         }}
       >
-        🎶 Activar sonido
+        Comenzar
       </motion.button>
     </motion.div>
   )}
@@ -175,15 +175,15 @@ const [noPosition, setNoPosition] = useState({ x: 0, y: 80 });
               opacity: [0.1, 0.3, 0.1],
             }}
             transition={{
-              duration: Math.random() * 10 + 15,
+              duration: Math.random() * 20 + 25,
               repeat: Infinity,
-              delay: Math.random() * 5,
+              delay: Math.random() * 0.5,
               ease: "linear",
             }}
           >
             <Heart
               className="text-pink-400"
-              size={Math.random() * 30 + 20}
+              size={Math.random() * 40 + 30}
               fill="currentColor"
             />
           </motion.div>
@@ -199,11 +199,11 @@ const [noPosition, setNoPosition] = useState({ x: 0, y: 80 });
         <Heart className="text-red-500 mx-auto mb-6" size={60} fill="currentColor" />
 
         <h1 className="text-5xl md:text-6xl font-bold text-red-600 mb-6">
-          Desde que llegaste a mi vida
+          Desde que llegaste a mi vida, me hiciste el hombre mas feliz del mundo y cada día a tu lado es un regalo que atesoro con todo mi corazón.
         </h1>
 
         <p className="text-2xl text-rose-700 max-w-4xl mx-auto">
-          y aqui un pequeño recuento de lo que eh vivido con el amor de mi vida y la persona a la que mas amo en este mundo
+          Y aqui un pequeño recuento de lo que eh vivido con el amor de mi vida y la persona a la que mas amo en este mundo.
         </p>
 
         <div className="mt-8 flex justify-center gap-3">
@@ -221,21 +221,25 @@ const [noPosition, setNoPosition] = useState({ x: 0, y: 80 });
       </div>
 
       {/* Closing */}
+      
       <div className="max-w-4xl mx-auto px-6 pb-32 text-center">
-        
+        {!accepted && !showQuestion &&(
         <p className="text-2xl text-rose-700 max-w-4xl mx-auto">
          fue un año lleno de emociones, aventuras y momentos inolvidables, asi que este año vengo e nuevo a preguntarte...
         </p>
+        )}
+        
         <br></br>
         <br></br>
-        <motion.button
+        {!accepted && !showQuestion &&(
+          <motion.button
           className="bg-gradient-to-r from-pink-500 to-red-600 text-white px-12 py-6 rounded-full text-3xl font-bold"
           whileHover={{ scale: 1.1 }}
           onClick={() => setShowQuestion(!showQuestion)}
         >
           💕 Haz clic aquí 💕
         </motion.button>
-        
+         )}
         <AnimatePresence>
   {showQuestion && (
     <motion.div
@@ -244,9 +248,13 @@ const [noPosition, setNoPosition] = useState({ x: 0, y: 80 });
       animate={{ opacity: 1 }}
       exit={{ opacity: 0 }}
     >
+
+    {!accepted &&(
       <h2 className="text-4xl font-bold text-red-600 text-center">
         ¿Quisieras volver a ser mi San Valentín este año? 💖
       </h2>
+    )}
+      
 
       {!accepted ? (
         <div className="relative flex gap-8 mt-6">
@@ -284,6 +292,9 @@ const [noPosition, setNoPosition] = useState({ x: 0, y: 80 });
           gracias por hacerme el hombre más feliz del mundo y por permitirme ser tu san valentin de nuevo 
           
           Te super Mega AMO💖
+          <br></br>
+          <br></br>
+          Tenemos una cita pendiente para celebrar nuestro amor y hacer que este año sea aún más especial que el anterior. Te amo con todo mi corazón, mi amorcito preciosa 😘.
 
         </motion.p>
       )}
